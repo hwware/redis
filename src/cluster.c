3c9ca151af57d70c19a3eb7c781d8d030cca4688
Wen Hui <wen.hui.ware@gmail.com>
2021-09-24 13:36:18 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -813,6 +813,7 @@ void setClusterNodeName(clusterNode *node){
     int post_digits = floor(log10(abs(node->port))) + 1;
     name = malloc(strlen(node->ip) + post_digits + 2);
     sprintf(name, "%s%s%d", node->ip, "_", node->port);
+    serverLog(LL_WARNING, "----------changed to : %s", name);
     node->hname = name;
 }
 
