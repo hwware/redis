72748efb674673a1c70c5cc68f537ea41529ba13
Wen Hui <wen.hui.ware@gmail.com>
2021-09-24 13:48:58 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -813,7 +813,9 @@ void setClusterNodeName(clusterNode *node){
     serverLog(LL_WARNING, "-----------------Started setClusterNodeName");
     char *name;
     int post_digits = floor(log10(abs(node->port))) + 1;
-    name = zmalloc(strlen(node->ip) + post_digits + 2);
+    int allocate_len = strlen(node->ip) + post_digits + 2
+    serverLog(LL_WARNING, "-----------------Len to alloc = %d", allocate_len);
+    name = zmalloc(allocate_len);
     sprintf(name, "%s%s%d", node->ip, "_", node->port);
     serverLog(LL_WARNING, "----------changed to : %s", name);
     node->hname = name;
