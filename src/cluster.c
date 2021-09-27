a920b2bbf10f8a883e4be2a66706aa6ffe2b38b9
Wen Hui <wen.hui.ware@gmail.com>
2021-09-27 10:17:22 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -4253,7 +4253,7 @@ sds clusterGenNodeDescription(clusterNode *node, int use_pport) {
     /* Node coordinates */
     ci = sdscatlen(sdsempty(),node->name,CLUSTER_NAMELEN);
 
-    ci = sdscatfmt(ci," %s ",node->hname); 
+    // ci = sdscatfmt(ci," %s ",node->hname); 
     ci = sdscatfmt(ci," %s:%i@%i ",
         node->ip,
         port,
