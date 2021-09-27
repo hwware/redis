7a24f5596811094ef54921fe95616c2b91674097
Wen Hui <wen.hui.ware@gmail.com>
2021-09-27 17:00:23 -0400

    Add hostname variable

--- src/cluster.h
+++ src/cluster.h
@@ -113,6 +113,7 @@ typedef struct clusterNodeFailReport {
 typedef struct clusterNode {
     mstime_t ctime; /* Node object creation time. */
     char name[CLUSTER_NAMELEN]; /* Node name, hex string, sha1-size */
+    char* hname; /* Human readable name for node */
     int flags;      /* CLUSTER_NODE_... */
     uint64_t configEpoch; /* Last configEpoch observed for this node */
     unsigned char slots[CLUSTER_SLOTS/8]; /* slots handled by this node */
