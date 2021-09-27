f27cd4302211f4afd2e5f8d9b4c1d813292f6bce
Wen Hui <wen.hui.ware@gmail.com>
2021-09-27 16:38:43 -0400

    Revert "Update cluster.c"
    This reverts commit e95f4cfe66ffb44293c3835e900873605ce5c094.

--- src/cluster.c
+++ src/cluster.c
@@ -1082,15 +1082,7 @@ clusterNode *clusterLookupNode(const char *name) {
 
     de = dictFind(server.cluster->nodes,s);
     sdsfree(s);
-    if (de == NULL){
-        di = dictGetSafeIterator(server.cluster->nodes);
-        while((de = dictNext(di)) != NULL) {
-            clusterNode *node = dictGetVal(de);
-            if (strcmp(name,node->hname) == 0) return node;
-        }
-        dictReleaseIterator(di);
-        return NULL;
-    }
+    if (de == NULL) return NULL;
     return dictGetVal(de);
 }
 
