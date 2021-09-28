f25755c42dbf2ca08af7e35b893a75da07d42b35
Wen Hui <wen.hui.ware@gmail.com>
2021-09-28 10:14:18 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -1210,8 +1210,10 @@ clusterNode *clusterLookupNode(const char *name) {
         di = dictGetSafeIterator(server.cluster->nodes);
         while((de2 = dictNext(di)) != NULL) {
             clusterNode *node = dictGetVal(de2);
-            if (strcmp(node->hname,name ) == 0)
-                return node;
+            if (node->hname){
+                if (strcmp(node->hname,name ) == 0)
+                    return node;
+            }
         }
         dictReleaseIterator(di);
         return NULL;
