3f9940142f037ac79a00e3e52692fcbd665a0098
Wen Hui <wen.hui.ware@gmail.com>
2021-09-28 15:59:46 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -926,7 +926,7 @@ unsigned int keyHashSlot(char *key, int keylen) {
  * -------------------------------------------------------------------------- */
 
 /* Assign a human readable name to nodes for clusters*/
-void setClusterNodeName(clusterNode *node){
+void setClusterNodeName(clusterNode *node) {
     char *name;
     int post_digits;
     if (node->port == 0){
