ed5665f0e4174e75256dc50408b932611a126f7b
Wen Hui <wen.hui.ware@gmail.com>
2021-10-04 12:38:32 -0400

    Update cluster.tcl

--- tests/cluster/cluster.tcl
+++ tests/cluster/cluster.tcl
@@ -12,7 +12,7 @@ set ::cluster_replica_nodes 0
 proc get_cluster_nodes id {
     puts "============================ In get cluster nodes ================================"
     set lines [split [R $id cluster nodes] "\r\n"]
-    puts lines
+    puts $lines
     set nodes {}
     foreach l $lines {
         set l [string trim $l]
