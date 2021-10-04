499ee0e3afcc38aeca4e5d3acd00ace42c7530d4
Wen Hui <wen.hui.ware@gmail.com>
2021-10-04 12:23:12 -0400

    Update 00-base.tcl

--- tests/cluster/tests/00-base.tcl
+++ tests/cluster/tests/00-base.tcl
@@ -12,6 +12,8 @@ test "Different nodes have different IDs" {
     set ids {}
     set numnodes 0
     foreach_redis_id id {
+        puts " Entered for loop"
+        puts id
         incr numnodes
         # Every node should just know itself.
         set nodeid [dict get [get_myself $id] id]
