89329de1851ced3b15c2f9e9139ae238efa8ac1d
Wen Hui <wen.hui.ware@gmail.com>
2021-10-04 12:52:42 -0400

    Update cluster.tcl

--- tests/cluster/cluster.tcl
+++ tests/cluster/cluster.tcl
@@ -18,7 +18,7 @@ proc get_cluster_nodes id {
         set l [string trim $l]
         if {$l eq {}} continue
         set args [split $l]
-        puts [lindex $args 0]
+        puts [lindex $args 2]
         set node [dict create \
             id [lindex $args 0] \
             name [lindex $args 1] \
