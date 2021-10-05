5d3a91526592c8841776f0f61629576cb4f4b7b0
Wen Hui <wen.hui.ware@gmail.com>
2021-10-05 11:21:58 -0400

    Added support for hname in CLUSTER NODES

--- tests/cluster/cluster.tcl
+++ tests/cluster/cluster.tcl
@@ -18,14 +18,15 @@ proc get_cluster_nodes id {
         set args [split $l]
         set node [dict create \
             id [lindex $args 0] \
-            addr [lindex $args 1] \
-            flags [split [lindex $args 2] ,] \
-            slaveof [lindex $args 3] \
-            ping_sent [lindex $args 4] \
-            pong_recv [lindex $args 5] \
-            config_epoch [lindex $args 6] \
-            linkstate [lindex $args 7] \
-            slots [lrange $args 8 end] \
+            name [lindex $args 1] \
+            addr [lindex $args 2] \
+            flags [split [lindex $args 3] ,] \
+            slaveof [lindex $args 4] \
+            ping_sent [lindex $args 5] \
+            pong_recv [lindex $args 6] \
+            config_epoch [lindex $args 7] \
+            linkstate [lindex $args 8] \
+            slots [lrange $args 9 end] \
         ]
         lappend nodes $node
     }
