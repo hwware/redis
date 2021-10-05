928287a0694653fdbcc3a30cd119fe610ce1768a
Wen Hui <wen.hui.ware@gmail.com>
2021-10-05 11:21:58 -0400

    Added support for hname in CLUSTER NODES

--- src/cluster.c
+++ src/cluster.c
@@ -277,7 +277,7 @@ int clusterLoadConfig(char *filename) {
          * stored in nodes.conf. It is received later over the bus protocol. */
 
         /* Parse flags */
-        p = s = argv[2];
+        p = s = argv[offset + 2];
         while(p) {
             p = strchr(s,',');
             if (p) *p = '\0';
