09f92f420d2ac98c978a5123413dcaf58955f22e
Wen Hui <wen.hui.ware@gmail.com>
2021-10-13 14:12:26 -0400

    Revert "Rebase and remove extra lines"
    This reverts commit 65c9208b0fa963a9953c154a856befe750b4b45b.

--- src/server.c
+++ src/server.c
@@ -6898,6 +6898,7 @@ void infoCommand(client *c) {
     }
 
     if (c->argc == 1) {
+
         sds info = genRedisInfoString("default");
         addReplyVerbatim(c,info,sdslen(info),"txt");
         sdsfree(info);
@@ -6907,11 +6908,13 @@ void infoCommand(client *c) {
     int defsections = 0, allsections = 0;
     // first time find all/default flag
     for (int i = 1; i < c->argc; i++) {
+
         defsections = !strcasecmp(c->argv[i]->ptr,"default");
         allsections = !strcasecmp(c->argv[i]->ptr,"all");
     }
 
     if (defsections || allsections) {
+
         sds info = allsections ? genRedisInfoString("all") : genRedisInfoString("default");
         addReplyVerbatim(c,info,sdslen(info),"txt");
         sdsfree(info);
@@ -6922,6 +6925,7 @@ void infoCommand(client *c) {
     int lastValid = 0; 
     // second time parse specific section flag
     for (int i = 1; i < c->argc; i++) {
+
         if (lastValid) {
             info = sdscat(info,"\r\n");
         }
