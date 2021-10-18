cf75e1cd693a2b114a92c6044fad7ba238c0610a
Wen Hui <wen.hui.ware@gmail.com>
2021-10-18 15:24:32 -0400

    Update server.c

--- src/server.c
+++ src/server.c
@@ -6121,8 +6121,8 @@ sds genRedisInfoString(const char *section) {
 }
 
 void infoCommand(client *c) {
-    char * defCommands[11][15] = {"server", "clients", "memory", "persistence", "stats", "replication", "cpu", "modules", "errorstats", "cluster", "keyspace"};
-    char * allCommands[12][15] = {"server", "clients", "memory", "persistence", "stats", "replication", "cpu", "modules", "errorstats", "cluster", "keyspace", "commandstats"};
+    char ** defCommands[11][15] = {"server", "clients", "memory", "persistence", "stats", "replication", "cpu", "modules", "errorstats", "cluster", "keyspace"};
+    char ** allCommands[12][15] = {"server", "clients", "memory", "persistence", "stats", "replication", "cpu", "modules", "errorstats", "cluster", "keyspace", "commandstats"};
     robj * final = setTypeCreate(NULL);
 
     if (c->argc == 1) {
