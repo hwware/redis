f6ff62ef69d3de830bc8527db47fdf1a88a69704
Wen Hui <wen.hui.ware@gmail.com>
2021-10-22 14:52:46 -0400

    debugging cluster test tcl

--- tests/cluster/tests/includes/init-tests.tcl
+++ tests/cluster/tests/includes/init-tests.tcl
@@ -58,6 +58,7 @@ test "Cluster Join and auto-discovery test" {
 
     foreach_redis_id id {
         wait_for_condition 1000 50 {
+            puts $ids
             [llength [get_cluster_nodes $id]] == [llength $ids]
         } else {
             fail "Cluster failed to join into a full mesh."
