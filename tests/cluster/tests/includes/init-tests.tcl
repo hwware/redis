8a284774662e027bd296ad89e3e49fa5bbef2738
Wen Hui <wen.hui.ware@gmail.com>
2021-10-25 12:59:29 -0400

    Debugging get_cluster_node

--- tests/cluster/tests/includes/init-tests.tcl
+++ tests/cluster/tests/includes/init-tests.tcl
@@ -61,8 +61,8 @@ test "Cluster Join and auto-discovery test" {
         puts $id
         puts "IDS is"
         puts $ids
-        puts "get_cluster_nodes"
-        puts [get_cluster_nodes $id]
+        # puts "get_cluster_nodes"
+        # puts [get_cluster_nodes $id]
         wait_for_condition 1000 50 {
             [llength [get_cluster_nodes $id]] == [llength $ids]
         } else {
