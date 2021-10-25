08170a77bee6e328ebe8f457a9b1f53482b85f6b
Wen Hui <wen.hui.ware@gmail.com>
2021-10-25 16:11:44 -0400

    Update cluster.h

--- src/cluster.h
+++ src/cluster.h
@@ -215,6 +215,7 @@ typedef struct {
     uint16_t flags;             /* node->flags copy */
     uint16_t pport;             /* plaintext-port, when base port is TLS */
     uint16_t notused1;
+    int custom_name;
 } clusterMsgDataGossip;
 
 typedef struct {
