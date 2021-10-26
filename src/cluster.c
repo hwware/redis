8ac5751c0a780a793c19e397f633114ba6b892c0
Wen Hui <wen.hui.ware@gmail.com>
2021-10-26 14:54:38 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -2595,6 +2595,7 @@ void clusterBuildMessageHdr(clusterMsg *hdr, int type) {
     hdr->sig[3] = 'b';
     hdr->type = htons(type);
     memcpy(hdr->sender,myself->name,CLUSTER_NAMELEN);
+    serverLog("GENERATING HDR ADDING %s TO HDR->HNAME", myself->hname);
     strncpy(hdr->hname,myself->hname,strlen(myself->hname));
 
     /* If cluster-announce-ip option is enabled, force the receivers of our
