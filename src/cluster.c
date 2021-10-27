0287df6dd9125c3f3aaa195bcb8544a2fe2bca21
Wen Hui <wen.hui.ware@gmail.com>
2021-10-26 20:34:59 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -822,7 +822,7 @@ void setClusterNodeName(clusterNode *node) {
         return;
     char ip[strlen(node->ip)];
     strcpy(ip,node->ip);
-    snprintf(node->hname, "%u", (unsigned int)node->port);
+    sprintf(node->hname, "%u", (unsigned int)node->port);
 }
 
 /* Manually assign a human readable name to nodes for clusters*/
