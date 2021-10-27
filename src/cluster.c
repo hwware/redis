64c4eab78f101d2bc214ece91447e3f2c48a0d26
Wen Hui <wen.hui.ware@gmail.com>
2021-10-26 20:21:57 -0400

    Update cluster.c

--- src/cluster.c
+++ src/cluster.c
@@ -820,9 +820,9 @@ unsigned int keyHashSlot(char *key, int keylen) {
 void setClusterNodeName(clusterNode *node) {
     if (node->custom_name == 1)
         return;
-    strcpy(node->hname,node->ip);
-    strcat(node->hname, "_");
-    strcat(node->hname, node->port);
+    char ip[strlen(node->ip)];
+    strcpy(ip,node->ip);
+    snprintf(node->hname, "%s_%s",ip, node->port);
 }
 
 /* Manually assign a human readable name to nodes for clusters*/
