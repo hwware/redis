f99bb3ac0712dc28fe61c6c328e91dd02b80b92d
Wen Hui <wen.hui.ware@gmail.com>
2021-11-02 14:35:46 -0400

    Formatting change

--- src/server.c
+++ src/server.c
@@ -6898,7 +6898,7 @@ void infoCommand(client *c) {
     }
     dictAdd(allSet, sdsnew("commandstats"), NULL);
 
-    /* When info is called with no other arguments*/
+    /* When info is called with no other arguments */
     if (c->argc == 1) {
         sds info = genRedisInfoString("default");
         addReplyVerbatim(c,info,sdslen(info),"txt");
