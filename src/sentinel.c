d291f8d4fe28027d19ae28c6b7a93947b75e6178
Wen Hui <wen.hui.ware@gmail.com>
2021-11-11 13:01:47 -0500

    Update sentinel.c

--- src/sentinel.c
+++ src/sentinel.c
@@ -4166,7 +4166,7 @@ void sentinelGetCommand(client *c) {
     // serverLog(LL_WARNING, " first argument %s, second argument %s, third argument %s", c->argv[1]->ptr, c->argv[2]->ptr, c->argv[3]->ptr);
 
     if ((ri = sentinelGetMasterByNameOrReplyError(c,c->argv[2])) == NULL) return;
-    serverLog("FOUND MASTER : %s", ri->name);
+    serverLog(LL_WARNING, "FOUND MASTER : %s", ri->name);
 
     sds ci = sdsempty();
     
