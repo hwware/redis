7cfaa68ee72834bb59874f88d64a0a88e4827651
Wen Hui <wen.hui.ware@gmail.com>
2021-11-11 15:48:10 -0500

    Update sentinel.c

--- src/sentinel.c
+++ src/sentinel.c
@@ -4172,31 +4172,28 @@ void sentinelGetCommand(client *c) {
 
     if (!strcasecmp(option,"down-after-milliseconds") || has_get_all) {
         /* down-after-milliseconds <milliseconds> */
-        ci = sdscatprintf(ci, "sentinel down-after-milliseconds %llu '\r\n'", ri->down_after_period);
-        // addReplyBulkCString(c,"sentinel down-after-milliseconds");
-        // addReplyLongLong(c, ri->down_after_period);
+        ci = sdscatprintf(ci, "sentinel down-after-milliseconds %llu\r\n", ri->down_after_period);
     }
     if (!strcasecmp(option,"failover-timeout") || has_get_all) {
         /* failover-timeout <milliseconds> */
-        ci = sdscatprintf(ci, "sentinel failover-timeout %llu \r\n", ri->failover_timeout);
-        ci = sdscat(ci,"\r\n");
+        ci = sdscatprintf(ci, "sentinel failover-timeout %llu\r\n", ri->failover_timeout);
     }
     if (!strcasecmp(option,"parallel-syncs") || has_get_all) {
         /* parallel-syncs <milliseconds> */
-        ci = sdscatprintf(ci, "sentinel parallel-syncs %s", 
+        ci = sdscatprintf(ci, "sentinel parallel-syncs %s\r\n", 
             ri->parallel_syncs ? "yes" : "no");
     }
     if (!strcasecmp(option,"quorum") || has_get_all) {
         /* quorum <count> */
-        ci = sdscatprintf(ci, "sentinel quorum %d", ri->quorum);
+        ci = sdscatprintf(ci, "sentinel quorum %d\r\n", ri->quorum);
     }
     if (!strcasecmp(option,"runid") || has_get_all) {
         /* quorum <count> */
-        ci = sdscatprintf(ci, "sentinel runid %s", ri->runid);
+        ci = sdscatprintf(ci, "sentinel runid %s\r\n", ri->runid);
     }
     if (!strcasecmp(option,"deny-scripts-reconfig") || has_get_all) {
         /* quorum <count> */
-        ci = sdscatprintf(ci, "sentinel config-epoch %lu", ri->config_epoch);
+        ci = sdscatprintf(ci, "sentinel config-epoch %lu\r\n", ri->config_epoch);
     }
     if (sdslen(ci) == 0) {
         addReplyErrorFormat(c,"Unknown option \nSENTINEL SET '%s'", option);
