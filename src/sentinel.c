a161d2187c76ac2cab43819df587c4fa7179f09d
Wen Hui <wen.hui.ware@gmail.com>
2021-11-12 15:07:55 -0500

    Update sentinel.c

--- src/sentinel.c
+++ src/sentinel.c
@@ -4164,7 +4164,7 @@ void sentinelGetCommand(client *c) {
     dictIterator *di;
     dictEntry *de;
     int matches = 0;
-    char *option;
+    char *option = NULL;
     int has_get_all = 0;
     void *replylen = addReplyDeferredLen(c);
 
