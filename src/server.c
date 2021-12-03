a3bc141c3d98c913b1999cebf1d6147e1662fba9
Wen <wen.hui.ware@gmail.com>
2021-12-03 21:20:19 +0000

    Fixed module err

--- src/server.c
+++ src/server.c
@@ -6184,6 +6184,7 @@ sds genRedisInfoString(client * c, const char * source) {
     int all_sections = 0;
     int everything = 0;
     int is_sentinel = 0;
+    int modules = 0;
     
     if (!strcasecmp(source,"sentinel"))
         is_sentinel = 1;
@@ -6200,8 +6201,10 @@ sds genRedisInfoString(client * c, const char * source) {
                     default_sections = 1;
                 } else if (!strcasecmp(c->argv[i]->ptr,"all")) {
                     all_sections = 1;
-                } else if (!strcasecmp(c->argv[i]->ptr,"everything")) {
+                }  else if (!strcasecmp(c->argv[i]->ptr,"everything")) {
                     everything = 1;
+                } else if (!strcasecmp(c->argv[i]->ptr,"modules")) {
+                    modules = 1;
                 } else {
                     sds section = sdsnew(c->argv[i]->ptr);
                     sdstolower(section);
@@ -6220,7 +6223,6 @@ sds genRedisInfoString(client * c, const char * source) {
     sds info = sdsempty();
     time_t uptime = server.unixtime-server.stat_starttime;
     int j;
-    int modules = 0;
     int sections = 0;
     
     if (everything) all_sections = 1;
@@ -6868,11 +6870,10 @@ sds genRedisInfoString(client * c, const char * source) {
     }
 
     /* Modules */
-    if (((default_sections || all_sections) && !is_sentinel) || (dictFind(section_dict,"modules") != NULL)) {
+    if (((default_sections || all_sections || modules) && !is_sentinel)) {
         if (sections++) info = sdscat(info,"\r\n");
         info = sdscatprintf(info,"# Modules\r\n");
         info = genModulesInfoString(info);
-        modules = 1;
     }
 
     /* Command statistics */
