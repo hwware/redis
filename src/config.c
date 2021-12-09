6508c54f45bc3d598112a558d48a52b7f6c51ba9
Wen Hui <wen.hui.ware@gmail.com>
2021-12-09 14:59:05 -0500

    fix grammar error

--- src/config.c
+++ src/config.c
@@ -771,7 +771,7 @@ void configSetCommand(client *c) {
                 /* Apply function not stored, store it */
                 if (!exists) {
                     apply_fns[j] = set_configs[i]->interface.apply;
-                    config_map_fns[j] = i
+                    config_map_fns[j] = i;
                 }
                     
             }
