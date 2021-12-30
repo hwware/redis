0822f80ac6af1c88da787d30a28d0af95c1d6eaa
hwware <wen.hui.ware@gmail.com>
2021-12-30 15:04:45 -0500

    update genInfoSectionDict interface

--- src/server.c
+++ src/server.c
@@ -6182,8 +6182,7 @@ void addSectionsToDict(dict *section_dict, char **sections, int len) {
     }
 }
 
-
-dict *genInfoSectionDict(client *c, const char *source, int *all_sections, int *everything) {
+dict *genInfoSectionDict(char **argv, int argc, int *out_all, int *out_everything) {
     char *defSections[] = {"server", "clients", "memory", "persistence", "stats", "replication", "cpu", "modules", "errorstats", "cluster", "keyspace"};
     char *defSectionsSentinel[] = {"server", "clients", "cpu", "stats"};
 
@@ -6231,7 +6230,6 @@ dict *genInfoSectionDict(client *c, const char *source, int *all_sections, int *
     }  
     return section_dict;
 }
-
 
 /* Create the string returned by the INFO command. This is decoupled
  * by the INFO command itself as we need to report the same information
@@ -6970,9 +6968,7 @@ void infoCommand(client *c) {
 
     int all_sections = 0;
     int everything = 0;
-
-    dict *sections_dict = genInfoSectionDict(c, "server", &all_sections, &everything);
-
+    dict *sections_dict = genInfoSectionDict(c->argv, c->argc, &all_sections, &everything);
     sds info = genRedisInfoString(sections_dict, all_sections, everything);
     addReplyVerbatim(c,info,sdslen(info),"txt");
     sdsfree(info);
