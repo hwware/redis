5242ee720696f4f7a0d166ae1d94e1cc542d123d
Shivshankar Reddy Sheri <shivshankar.84285360@huawei.com>
2023-02-28 22:17:14 +0000

    first version

--- src/t_list.c
+++ src/t_list.c
@@ -592,7 +592,8 @@ void llenCommand(client *c) {
 
 /* LINDEX <key> <index> */
 void lindexCommand(client *c) {
-    robj *o = lookupKeyReadOrReply(c,c->argv[1],shared.null[c->resp]);
+//    robj *o = lookupKeyReadOrReply(c,c->argv[1],shared.null[c->resp]);
+    robj *o = lookupKeyReadOrReply(c,c->argv[1],shared.nokeyerr);
     if (o == NULL || checkType(c,o,OBJ_LIST)) return;
     long index;
 
@@ -613,7 +614,8 @@ void lindexCommand(client *c) {
             addReplyBulkLongLong(c, lval);
         }
     } else {
-        addReplyNull(c);
+//        addReplyNull(c);
+        addReplyErrorObject(c,shared.outofrangeerr);
     }
 
     listTypeReleaseIterator(iter);
