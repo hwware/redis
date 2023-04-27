c64c44a0dc5a4b8e2e1a5ec79e9e580d78cc67cd
hwware <wen.hui.ware@gmail.com>
2023-04-27 18:01:24 +0000

    remove redundancy code

--- src/sentinel.c
+++ src/sentinel.c
@@ -5312,12 +5312,7 @@ void sentinelHandleRedisInstance(sentinelRedisInstance *ri) {
 
     /* Every kind of instance */
     sentinelCheckSubjectivelyDown(ri);
-
-    /* Masters and slaves */
-    if (ri->flags & (SRI_MASTER|SRI_SLAVE)) {
-        /* Nothing so far. */
-    }
-
+    
     /* Only masters */
     if (ri->flags & SRI_MASTER) {
         sentinelCheckObjectivelyDown(ri);
